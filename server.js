87d48ac4b7e96ac0849a2bf729896d274dc42ce8
Sreelekshmi V <sreelekshmi1771996@gmail.com>
2017-08-19 19:19:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/article1', function (req, res) {
+  res.send('Article one requested and will be served here');
+});
+
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
