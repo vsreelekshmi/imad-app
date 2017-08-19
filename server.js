143bd7fd477b37389c90fea27ba0f568916f7a6c
Sreelekshmi V <sreelekshmi1771996@gmail.com>
2017-08-19 20:48:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,14 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article1', function (req, res) {
-  res.send('Article one requested and will be served here');
+      res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
+//  res.send('Article one requested and will be served here');
+});
+app.get('/article2', function (req, res) {
+      res.sendFile(path.join(__dirname, 'ui', 'article2.html'));
+});
+app.get('/article3', function (req, res) {
+      res.sendFile(path.join(__dirname, 'ui', 'article3.html'));
 });
 
 
